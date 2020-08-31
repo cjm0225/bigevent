0c8ef1fd73c63f14bce6b6fb26def1a475c01ac9
cjm0225 <45387377+cjm0225@users.noreply.github.com>
2020-08-31 16:59:02 +0800

    增加导航栏点击效果和二级菜单显示

--- admin/js/index.js
+++ admin/js/index.js
@@ -2,7 +2,7 @@
 // 获取token，在每次获取权限的时候验证是否已经登录过
 const token = window.localStorage.getItem("bigevent_token");
 
-
+// 获取用于信息
 function getUserinfo() {
     $.ajax({
         type: "get",
@@ -11,20 +11,49 @@ function getUserinfo() {
             "Authorization": token
         },
         success: function (response) {
-            // console.log(response);
+
             const { code, msg, data } = response;
+
             if (code === 200) {
                 $(".user_info span").text(`欢迎 ${data.nickname}`);
                 $(".user_info img").prop("src", data.userPic);
                 $(".user_center_link img").prop("src", data.userPic);
+            }
+        },
+        error: function (response) {
+            // 当请求被拒绝的时候，表示未登录或者token凭证已过期
+            if (response.status === 403) {
+                // 清除token
+                window.localStorage.removeItem("bigevent_token");
+                //跳转到登录页面
+                window.location.href = "./login.html";
+
             }
         }
 
     });
 }
 
+getUserinfo();
+
+// 退出系统功能
 function logout() {
+    // 清除token
     window.localStorage.removeItem("bigevent_token");
+    //跳转到登录页面
     window.location.href = "./login.html";
 }
-getUserinfo(); 
+
+// 给左边导航栏一级菜单添加点击效果
+$(".level01").click(function () {
+    // 排他思想，被点击的显示效果，不被点击的不显示效果
+    $(".level01.active").removeClass("active");
+    $(this).addClass("active");
+});
+
+// 点击一级导航栏，显示二级菜单
+$(".level01").eq(1).click(function () {
+    // console.log($(".level02"));
+    $(".level02").stop().slideToggle();
+    $(".level01 b").toggleClass("rotate0");
+});
