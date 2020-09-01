96169d4bede2ef1980fb1acd56a689ae6929a8c5
cjm0225 <45387377+cjm0225@users.noreply.github.com>
2020-09-01 19:31:03 +0800

    用两种方法实现图片预览

--- admin/js/index.js
+++ admin/js/index.js
@@ -4,41 +4,12 @@ const token = window.localStorage.getItem("bigevent_token");
 
 // 获取用于信息
 function getUserinfo() {
-    // $.ajax({
-    //     type: "get",
-    //     url: "http://localhost:8080/api/v1/admin/user/info",
-    //     headers: {
-    //         "Authorization": token
-    //     },
-    //     success: function (response) {
-
-    //         const { code, msg, data } = response;
-
-    //         if (code === 200) {
-    //             $(".user_info span").text(`欢迎 ${data.nickname}`);
-    //             $(".user_info img").prop("src", data.userPic);
-    //             $(".user_center_link img").prop("src", data.userPic);
-    //         }
-    //     },
-    //     error: function (response) {
-    //         // 当请求被拒绝的时候，表示未登录或者token凭证已过期
-    //         if (response.status === 403) {
-    //             // 清除token
-    //             window.localStorage.removeItem("bigevent_token");
-    //             //跳转到登录页面
-    //             window.location.href = "./login.html";
-
-    //         }
-    //     }
-
-    // });
     request({
         type: "get",
         url: "/admin/user/info",
         success: function (response) {
-
             const { code, msg, data } = response;
-
+            // 将数据显示到页面中
             if (code === 200) {
                 $(".user_info span").text(`欢迎 ${data.nickname}`);
                 $(".user_info img").prop("src", data.userPic);
--- admin/js/user.js
+++ admin/js/user.js
@@ -7,6 +7,7 @@ function getUserDetail() {
         success: function (response) {
             if (response.code === 200) {
                 const { username, nickname, email, userPic, password } = response.data;
+                // 将数据显示到个人信息中
                 $("#username").val(username);
                 $("#nickname").val(nickname);
                 $("#email").val(email);
@@ -18,3 +19,33 @@ function getUserDetail() {
     });
 }
 getUserDetail();
+
+
+// // 用户图片编辑显示预览(读取本地文件的方法)
+// $("#exampleInputFile").change(function (e) {
+//     e.preventDefault();
+//     // 原生dom元素input中有files属性，是一个数组，存储一个或者多个文件信息(file对象)
+//     let localFile = $("#exampleInputFile")[0].files[0];
+
+//     // FileReader对象允许Web应用程序异步读取存储在用户计算机上的文件
+//     let reader = new FileReader();
+
+//     // 以url格式读取文件信息，无返回值
+//     reader.readAsDataURL(localFile, "UTF-8");
+
+//     // onload事件表示读取文件完成
+//     reader.onload = function (event) {
+//         $("#userPic").prop("src", event.target.result);
+//     }
+// });
+
+// 使用URL.createObectUrl（）方法实现图片编辑显示预览
+$("#exampleInputFile").change(function (e) {
+    e.preventDefault();
+    // 原生dom元素input中有files属性，是一个数组，存储一个或者多个文件信息(file对象)
+    let file = $("#exampleInputFile")[0].files[0];
+    // 使用URL.createObjectURL临时创建url，生存期绑定到创建该文档的窗口中的文档（就是说网页关闭时就无效或者说是被清除）
+    const url = URL.createObjectURL(file);
+    // 显示预览图片
+    $("#userPic").prop("src", url);
+});
