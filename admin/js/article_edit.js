2da202a386b0f30b77521a15dd99e049a3fab61a
cjm0225 <45387377+cjm0225@users.noreply.github.com>
2020-09-05 15:35:07 +0800

    解决文章分页点击后不刷新页数的bug

--- admin/js/article_edit.js
+++ admin/js/article_edit.js
@@ -1,6 +1,11 @@
+// 实现编辑文章功能
 function articleEdit() {
     // 从url获取id，location.search可以获取url参数，格式:?id=
     const id = Number(location.search.split("=")[1]);
+    request({
+        type: "post",
+        url: "/admin/article/edit",
 
+    });
 }
 articleEdit();
--- admin/js/article_list.js
+++ admin/js/article_list.js
@@ -19,7 +19,7 @@ getArticleCategory();
 
 
 
-
+const global = {};
 
 // 实现文章获取功能
 function article_query(paramObj) {
@@ -34,9 +34,11 @@ function article_query(paramObj) {
             if (response.code === 200) {
                 const data = response.data.data;
 
+
                 // 保存总页数和文章总数量
                 totalPages = response.data.totalPage;
                 totalCount = response.data.totalCount;
+
 
                 // 实现文章分页功能
                 $("#pagination").twbsPagination({
@@ -47,24 +49,35 @@ function article_query(paramObj) {
                     prev: "上一页",
                     next: "下一页",
                     onPageClick: function (e, page) {
-                        let id;
-                        $("#selCategory option").each(function () {
-                            if ($(this).prop("selected") === true) {
-                                id = $(this).attr("data-id");
-                            }
+                        // 因为插件的onPageClick方法会在初始化的时候就会自动调用一次这个函数，根据情况不需要第一次就获取数据，所以第一次要关闭获取数据
+                        if (global.bol) {
+                            // 把被点击的页数保存在全局变量中，以便于后面的获取
+                            global.page = page;
 
-                        });
-                        const type = id;
-                        const state = $("#selStatus").val();
+                            // 获取被选中的下拉框的自定义属性id
+                            let categoryId;
+                            $("#selCategory option").each(function () {
+                                if ($(this).prop("selected") === true) {
+                                    categoryId = $(this).attr("data-id");
+                                }
 
-                        // 实现页数搜索
-                        article_query({ type, state, page });
+                            });
+
+                            const type = categoryId;
+                            const state = $("#selStatus").val();
+
+                            // 实现页数搜索
+                            article_query({ type, state, page });
+
+                        }
                     }
                 });
+
                 // // 将数据传入template模板引擎中
                 const articleHtml = template("article_list", { data });
                 // // 将模板字符串渲染到页面
                 $(".table tbody").html(articleHtml);
+                global.bol = true;
             }
 
         }
@@ -75,18 +88,65 @@ article_query({});
 
 // 根据文章类型id和文章状态搜索文章
 $("#btnSearch").click(function (e) {
+    // 由于twbsPagination.js插件在再次请求数据时不刷新页数，需要把上一次初始化数据清空并解绑，再次请求重新初始化twbsPagination.js就可以解决页数不刷新的问题了
+    $('#pagination').empty();
+    $('#pagination').removeData('twbs-pagination');
+    $('#pagination').unbind('page');
+
     e.preventDefault();
     // 不能用return停止搜索全部,使用for循坏可以减少查询次数
-    let id;
+    let categoryId;
     $("#selCategory option").each(function () {
         if ($(this).prop("selected") === true) {
-            id = $(this).attr("data-id");
+            categoryId = $(this).attr("data-id");
         }
 
     });
-    const type = id;
+    const type = categoryId;
     const state = $("#selStatus").val();
     article_query({ type, state });
+});
+
+// // 文章删除函数
+function deleteArticle(id) {
+    request({
+        type: "post",
+        url: "/admin/article/delete",
+        data: { id },
+        success: function (response) {
+            if (response.code === 204) {
+
+
+                let categoryId;
+                $("#selCategory option").each(function () {
+                    if ($(this).prop("selected") === true) {
+                        categoryId = $(this).attr("data-id");
+                    }
+
+                });
+
+                const type = categoryId;
+                const state = $("#selStatus").val();
+
+                const page = global.page;
+
+                // 删除文章的时候，希望当前数据是在删除页中重新渲染
+                article_query({ type, state, page });
+
+                alert(response.msg);
+            }
+
+        }
+    });
+}
+
+// // 实现文章删除功能
+$(".table tbody").on("click", ".btn-danger", function (e) {
+    e.preventDefault();
+    const id = $(this).parents("tr").attr("data-id");
+    if (confirm("确认要删除吗？")) {
+        deleteArticle(id);
+    }
 });
 
 
