e45ad1c73e4ca4c93159c19ee0e5f639defd4baa
cjm0225 <45387377+cjm0225@users.noreply.github.com>
2020-09-05 16:07:17 +0800

    实现图片预览和文章类型获取功能

--- admin/js/article_release.js
+++ admin/js/article_release.js
@@ -9,3 +9,31 @@ tinymce.init({
     selector: "#richText",
     language: 'zh_CN',
 })
+
+// 文章类型获取并渲染
+function getArticleList() {
+    request({
+        type: "get",
+        url: "/admin/category/list",
+        success: function (response) {
+            if (response.code === 200) {
+                const { data } = response;
+                // 绑定模板引擎并传入数据
+                const categoryHTML = template("categoryHTML", { data });
+                // 渲染到页面
+                $("#categoryId").html(categoryHTML);
+            }
+        }
+    });
+}
+getArticleList();
+
+// 实现图片上传回显功能
+$("#inputCover").change(function () {
+    // 获取file对象
+    const file = $(this)[0].files[0];
+    // 创建临时url
+    const url = URL.createObjectURL(file);
+    //渲染显示
+    $(".article_cover").prop("src", url);
+})
